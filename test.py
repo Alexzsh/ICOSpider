764abd80e1d0ba9674f0716db2a26177381da62a
zjutzsh <zjutzsh@gmail.com>
2018-06-13 14:02:17 +0800

    ico without tiwtter

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import urllib.request
 import re
 from lxml import etree
 import json,time
+import gevent
+from gevent import monkey
+# monkey.patch_all
 header = {
 
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
@@ -41,8 +44,9 @@ class ICOTerm:
         self.etherscan  =term_dict['???']
         self.telegram   =term_dict['telegram']
         self.result={}
-    def getResponse(self,url,decoding=False,headers=header):
-        # start=time.time()
+        gevent.sleep(0)
+    def getResponse(self,url,decoding=False,vpn=True,headers=header):
+        start=time.time()
         request = urllib.request.Request(url, headers=header)
         try:
             response = opener.open(request)
@@ -50,9 +54,11 @@ class ICOTerm:
         except Exception as e:
             print("exception",e)
             return
-        # print("url ",url,"read over",(time.time()-start))
+        print("url %s read over %0.2fs" %(url,time.time()-start))
+        # print("self.result",self.result)
         if decoding:
             data.decode('utf-8')
+
         return data
 
     def GetYoutubeViewCount(self):
@@ -63,19 +69,26 @@ class ICOTerm:
             Tag = "YoutubeChannelViewCount"
             id = re.findall("(?<=/)[A-Za-z0-9_-]+$", YouTubeurl)[0];
             key = "AIzaSyAZj7k542xJe9zZSX0Boka858oZ_1RSlC4"
+            url = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id='
             # id = "UCbzLOZaEegUfTVBJQeSc0sw"
-            if ("channel" in YouTubeurl):
-                url = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id='
-                # url = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&id='+id+"&key=AIzaSyAZj7k542xJe9zZSX0Boka858oZ_1RSlC4"
-                "https://www.googleapis.com/youtube/v3/videos"
-            else:
+            if ("channel" not in YouTubeurl):
                 # url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id=' + id + "&key=AIzaSyAZj7k542xJe9zZSX0Boka858oZ_1RSlC4"
-                url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id='
-                Tag = "YoutubeVideoViewCount"
+                request = urllib.request.Request(YouTubeurl)
+                response = urllib.request.urlopen(request)
 
+                data = response.read().decode('utf-8')
+
+                path = '// meta[ @ property = "og:url"] /@content'
+                selector = etree.HTML(data)
+                data = selector.xpath(path)
+                if (len(data) == 0):
+                    raise Exception("Youtube域名出错")
+                else:
+                    YouTubeurl = data[0]
+            id = re.findall("(?<=/)[A-Za-z0-9_-]+$", YouTubeurl)[0];
             # url = url + id + "&key=AIzaSyAZj7k542xJe9zZSX0Boka858oZ_1RSlC4"
 
-            data = self.getResponse(url + id + "&key=" + key,True)
+            data = self.getResponse(url + id + "&key=" + key, True)
             data = json.loads(data)
             data = data['items'][0]['statistics']['viewCount']
 
@@ -85,7 +98,7 @@ class ICOTerm:
             print(e)
             pass
         finally:
-            self.result[res]= resnum
+            self.result[res] = resnum
             print('youtu over')
 
     def GetEtherscanAddress(self):
@@ -94,8 +107,9 @@ class ICOTerm:
         selector = etree.HTML(data)
         item = selector.xpath("//table[@class='table']/tr[3]/td[2]/text()")
         data = re.sub("[^\d]", "", item[0])
-        self.result[ tag]= data
+        self.result[tag]= data
         print('ether over')
+        # gevent.sleep(0)
     def getAlexaRank(self):
         tag = "AlexaRank"
         url = ('http://data.alexa.com/data?cli=10&dat=snbamz&url=%s' % (self.web))
@@ -108,9 +122,10 @@ class ICOTerm:
         dict_res = {'GlobalRank': global_rank[0], 'ChineseRank': country_rank[0]}
         self.result[ tag]= dict_res
         print('alexa over')
+        # gevent.sleep(0)
     def getGitHubStar(self):
         tag = "GitHubStar";
-        data = self.getResponse(self.github)
+        data = self.getResponse(self.github,vpn=False)
         selector = etree.HTML(data)
         url = self.github.replace("https://github.com/", "");
         path = '//a[@href="/' + url.replace("https://github.com", "") + '/stargazers"]/text()'
@@ -119,6 +134,7 @@ class ICOTerm:
         data = re.sub("[^\d]", "", data[0])
         self.result[ tag]= data.strip()
         print('git over')
+        # gevent.sleep(0)
     def getTlegramChannelSize(self):
         tag = 'TlegramChannelSize'
         data = self.getResponse(self.telegram)
@@ -127,11 +143,35 @@ class ICOTerm:
         result = re.sub("[^\d]", "", result[0])
         self.result[ tag]= result
         print('tele over')
+        # gevent.sleep(0)
     #def callbackEvent(self):
         #print("callback start")
         #self.event.wait()
         #eve.rawlink(lambda : print(ico.result))
         #print("callback over")
+
+class JSONObject:
+    def __init__(self, d):
+        self.__dict__ = d
+data=json.loads(js,object_hook=JSONObject)
+ico_list=[item for item in data.__dict__.items()]
+global res
+res=[]
+def func(msg):
+    print('ico',msg)
+    ico=ICOTerm(msg)
+    res.append(ico)
+    # print(res[0].result)
+    #make a coroutines via gevent
+    gevent.joinall([
+                    gevent.spawn(ico.getTlegramChannelSize()),
+                    gevent.spawn(ico.GetEtherscanAddress()),
+                    gevent.spawn(ico.getAlexaRank()),
+                    gevent.spawn(ico.getGitHubStar())
+                   # gevent.spawn(ico.GetYoutubeViewCount())
+
+                   ])
+    print("-----------------\n",len(res), [i.result for i in res])
 if __name__=='__main__':
 
     # import aiohttp
@@ -151,24 +191,9 @@ if __name__=='__main__':
     # ]
     # loop.run_until_complete(asyncio.wait(tasks))  # 执行任务
     # loop.close()
-    import gevent
-    class JSONObject:
-        def __init__(self, d):
-            self.__dict__ = d
-    data=json.loads(js,object_hook=JSONObject)
-    ico_list=[item for item in data.__dict__.items()]
-    r=[]
-    def func(msg):
-        print('ico',msg)
-        ico=ICOTerm(msg)
-        r.append(ico)
-        #make a coroutines via gevent
-        gevent.joinall(gevent.spawn(ico.getAlexaRank()),
-                       gevent.spawn(ico.getGitHubStar()),
-                       gevent.spawn(ico.getTlegramChannelSize(),timeout=60),
-                       gevent.spawn(ico.GetEtherscanAddress(),timeout=60),
-                       gevent.spawn(ico.GetYoutubeViewCount())
-                       )
+
+
+
         # print(ico.result)
         # ico.GetYoutubeViewCount()
         # Tag, num = ico.GetYoutubeViewCount()
@@ -193,43 +218,13 @@ if __name__=='__main__':
     # func(ico_list[0][1].__dict__)
     # print((time.time()-start)*1000)
     #make a processPool for evert iten
-    from multiprocessing.dummy import Pool
-    import multiprocessing
+    from multiprocessing import Pool,Process
     import time
     start=time.time()
-    pool=Pool(2)
+    pool=Pool(4)
     for i in ico_list:
         pool.apply_async(func,(i[1].__dict__,))
     pool.close()
     pool.join()
-    print((time.time()-start))
-    #for result
-    gevent.sleep(10)
-    print([i.result for i in r])
-    # def inner_func(msg,i):
-    #     print("main_msg:",msg,"inner_msg:" ,i)
-    #     time.sleep(2)
-    #     return
-    #
-    # def func(msg):
-    #     print("msg:", msg)
-    #     pool=Pool(10)
-    #     for _ in range(100):
-    #         pool.apply_async(inner_func,(msg,_))
-    #     pool.close()
-    #     pool.join()
-    #     return ("end"+msg)
-    #
-    #
-    # if __name__ == "__main__":
-    #
-    #     pool = Pool(4)
-    #
-    #     res=[]
-    #     for i in range(4):
-    #         (pool.apply_async(func, (i,)) ) # 维持执行的进程总数为processes，当一个进程执行完毕后会添加新的进程进去
-    #
-    #     pool.close()
-    #     pool.join()
-    #      # 调用join之前，先调用close函数，否则会出错。执行完close后不会有新的进程加入到pool,join函数等待所有子进程结束
-    #     print("Sub-process(es) done.")
+    print("main process over %0.2fs"%(time.time()-start))
+    print(len(res), [i.result for i in res])
