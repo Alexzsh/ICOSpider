947210bbb17ae31750414f5e95ebbd6e9d9b3a60
alexzsh <zjutzsh@gmail.com>
2018-09-08 13:19:01 +0800

    :memo: Update reconstructe via async

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-﻿import urllib.request
+﻿import requests
 import re
-import collections
 from lxml import etree
-import time
 from multiprocessing import Pool
 from multiprocessing import Process
 from selenium import webdriver
 import json
-import asyncio,aiofile,aiohttp,logging
-# import stem
-# import stem.connection
-# from stem.control import Controller
-# from stem import Signal
+import asyncio,aiohttp,logging
+from aiofile import Writer,AIOFile
+from bs4 import BeautifulSoup
+import os
+from colorlog import ColoredFormatter
+
+logger = logging.getLogger()  # 创建一个logger对象
+logger.setLevel('INFO')
+BASIC_FORMAT = '%(asctime)s  %(filename)s : %(levelname)s  %(message)s'
+DATE_FORMAT = '%Y-%m-%d %A %H:%M:%S'
+formatter = logging.Formatter(BASIC_FORMAT, DATE_FORMAT)
+color_formatter = ColoredFormatter('%(log_color)s[%(module)-15s][%(funcName)-20s][%(levelname)-8s] %(message)s')
+chlr = logging.StreamHandler() # 输出到控制台的handler
+chlr.setFormatter(color_formatter)
+chlr.setLevel('INFO')  # 也可以不设置，不设置就默认用logger的level
+fhlr = logging.FileHandler('logger.log') # 输出到文件的handler
+fhlr.setFormatter(color_formatter)
+logger.addHandler(chlr)
+logger.addHandler(fhlr)
+import stem
+import stem.connection
+from stem.control import Controller
+from stem import Signal
 
 from threading import Thread
 #simulate a browser to be prevented
@@ -24,16 +40,16 @@ header = {
 }
 
 proxies={
-    "http" : "127.0.0.1:1086",
-    "https" : "127.0.0.1:1086"
+    "http" : "127.0.0.1:8118",
+    "https" : "127.0.0.1:8118"
 }
-#proxy="127.0.0.1:8118"
-# Build ProxyHandler object by given proxy
-proxy_support=urllib.request.ProxyHandler(proxies)
-# Build opener with ProxyHandler object
-opener = urllib.request.build_opener(proxy_support)
-# Install opener to request
-urllib.request.install_opener(opener)
+# #proxy="127.0.0.1:8118"
+# # Build ProxyHandler object by given proxy
+# proxy_support=urllib.request.ProxyHandler(proxies)
+# # Build opener with ProxyHandler object
+# opener = urllib.request.build_opener(proxy_support)
+# # Install opener to request
+# urllib.request.install_opener(opener)
 
 
 
@@ -42,6 +58,10 @@ urllib.request.install_opener(opener)
 driver = webdriver.PhantomJS(
     executable_path='./phantomjs',
 )
+
+class JSONObject():
+    def __init__(self,d):
+        self.__dict__=d
 
 #define a class
 class ICOTerm():
@@ -57,22 +77,22 @@ class ICOTerm():
         self.result['name'] = term_dict['name']
 
     # get request via urllib
-    def getResponse(self, url):
-        request = urllib.request.Request(url, headers=header)
-        # print('ip is :',urllib.request.urlopen(urllib.request.Request("http://icanhazip.com", headers=header)).read())
-        try:
-            data = urllib.request.urlopen(request).read()
-        except Exception as e:
-            print("exception", e)
-            return
-        return data
+    # def getResponse(self, url):
+    #     request = urllib.request.Request(url, headers=header)
+    #     # print('ip is :',urllib.request.urlopen(urllib.request.Request("http://icanhazip.com", headers=header)).read())
+    #     try:
+    #         data = urllib.request.urlopen(request).read()
+    #     except Exception as e:
+    #         print("exception", e)
+    #         return
+    #     return data
 
     async def getResponseAsync(self,url):
         async with aiohttp.ClientSession(headers=header) as session:
             async with session.get(url) as resp:
                 return await resp.text()
 
-    def GetYoutubeViewCount(self):
+    async def GetYoutubeViewCount(self):
         Tag = "YoutubeChannelViewCount"
         YouTubeurl = self.youtube
         if YouTubeurl == '':
@@ -86,20 +106,19 @@ class ICOTerm():
             # id = "UCbzLOZaEegUfTVBJQeSc0sw"
             if "channel" not in YouTubeurl:
                 # url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id=' + id + "&key=AIzaSyAZj7k542xJe9zZSX0Boka858oZ_1RSlC4"
-                data = self.getResponse(YouTubeurl).decode('utf-8')
+                data = await self.getResponseAsync(YouTubeurl)
 				# get data via xpath
                 path = '// meta[ @ property = "og:url"] /@content'
                 selector = etree.HTML(data)
                 data = selector.xpath(path)
                 if (len(data) == 0):
-                    raise Exception("Youtube域名出错")
+                    logging.error('youtube url is illegal')
                 else:
                     YouTubeurl = data[0]
 
             id = re.findall("(?<=/)[A-Za-z0-9_-]+$", YouTubeurl)[0];
             # url = url + id + "&key=AIzaSyAZj7k542xJe9zZSX0Boka858oZ_1RSlC4"
-            data = self.getResponse(url + id + "&key=" + key)
-            data = data.decode('utf-8')
+            data = await self.getResponseAsync(url + id + "&key=" + key)
             data = json.loads(data)
             if len(data['items']) == 0:
                 return
@@ -108,11 +127,12 @@ class ICOTerm():
             res = Tag
             resnum = data
         except Exception as e:
-            print(e)
+            logging.error(e)
             pass
         finally:
+            logging.info('{} youtube over {}'.format(self.youtube,resnum))
             self.result[res] = resnum
-            print('youtu over')
+
 
     # def GetEtherScanAddress(self):
     #     tag = "EtherScanAddress"
@@ -132,9 +152,6 @@ class ICOTerm():
             return
         url = ('http://data.alexa.com/data?cli=10&dat=snbamz&url=%s' % self.web)
         data = await self.getResponseAsync(url)
-        # data = data.decode('gbk')
-
-        from bs4 import BeautifulSoup
         soup = BeautifulSoup(data, 'lxml')
         dict_res = {}
         dict_res['global_rank']=0
@@ -144,6 +161,8 @@ class ICOTerm():
             dict_res['country_rank'] = soup.country['rank']
         except TypeError as e:
             logging.error(self.web+'has no rank')
+        self.result[tag] = dict_res
+        logging.info('{} alexa over {}'.format(self.web, dict_res))
         # selector = etree.XML(data)
         # global_rank = selector.xpath('//SD/REACH/@RANK')
         # country_rank = selector.xpath('//SD/COUNTRY/@RANK')
@@ -156,43 +175,98 @@ class ICOTerm():
         #     dict_res['GlobalRank'] = global_rank[0]
         # if len(country_rank) != 0:
         #     dict_res['COUNTRY_RANK'] = country_rank[0]
-        self.result[tag] = dict_res
-        print('alexa over', dict_res)
 
-    def getGitHubStar(self):
-        tag = "GitHubStar";
-        if self.github == '':
+    async def getGitHubStar(self):
+        tag = "GitHubStar"
+        res={}
+        if self.github.endswith('/'):
+            self.github = self.github[:-1]
+        if self.github == '' or len(self.github.split('/'))<5:
+            logging.error(self.github+' is not a repo url')
             return
-        data = self.getResponse(self.github)
-        if data is None:
-            return
-        data.decode('utf-8')
-        selector = etree.HTML(data)
+        data = await self.getResponseAsync(self.github)
+
+        rule = re.compile('\s+')
+        soup = BeautifulSoup(data, 'lxml')
         url = self.github.replace("https://github.com/", "");
-        path = '//a[@href="/' + url + '/stargazers"]/text()'
-        data = selector.xpath(path)
-        if len(data) == 0:
-            return
-        data = re.sub("[^\d]", "", data[0])
-        self.result[tag] = data.strip()
-        print('git over')
 
-    def getTelegramChannelSize(self):
+        res['stargazers'] = 0
+        res['watchers'] = 0
+        res['network/members'] = 0
+
+        def getNum(item):
+            item=soup.select('[href=/'+url+'/'+str(item)+']')
+            if len(item):
+                return re.sub(rule,'',item[0].string)
+            else:
+                return 0
+
+        for k,v in res.items():
+            res[k]=getNum(v)
+
+        item=soup.find_all(class_='num text-emphasized')
+        res['commits'] = re.sub(rule,'',item[0].string)
+        res['branches'] = re.sub(rule,'',item[1].string)
+        res['releases'] = re.sub(rule,'',item[2].string)
+        try:
+            res['contributors'] = re.sub(rule,'',item[3].string)
+        except TypeError as e:
+            logging.error('{},{}'.format(e,self.github))
+        item=soup.find_all(class_='Counter')
+        res['issues'] = re.sub(rule, '', item[0].string)
+        res['PullRequest'] = re.sub(rule, '', item[1].string)
+        res['Projects'] = re.sub(rule, '', item[2].string)
+
+
+        self.result[tag] = res
+        logging.info('{} git over {}'.format(self.github,res))
+
+        # res_json = json.loads(data, object_hook=JSONObject)
+        # try:
+        #     res['stargazers_count'] = res_json.stargazers_count
+        #     res['watchers_count'] = res_json.watchers_count
+        #     res['forks'] = res_json.forks
+        #     res['open_issues'] = res_json.open_issues
+        #     res['subscribers_count'] = res_json.subscribers_count
+        #     res['network_count'] = res_json.network_count
+        #     res['size'] = res_json.size
+        #     res['open_issues'] = res_json.open_issues
+        #     res['open_issues'] = res_json.open_issues
+        #     rule = re.compile(r'{[[/s/S].*?}')
+        #     comments = re.sub(rule, '', res_json.comments_url)
+        #     commits = re.sub(rule, '', res_json.commits_url)
+        #     contributors = res_json.contributors_url
+        #     subscribers = res_json.subscribers_url
+        # except AttributeError as e:
+        #     logging.error(e)
+        # wait={
+        #     'comments':comments,
+        #     'commits':commits,
+        #     'contributors':contributors,
+        #     'subscribers':subscribers
+        # }
+        # for k,v in wait.items():
+        #     res[k]=len(json.loads(await (self.getResponseAsync(v)), object_hook=JSONObject))
+        # self.result[tag]=res
+
+    async def getTelegramChannelSize(self):
         tag = 'TelegramChannelSize'
         if self.telegram == '':
             return
-        data = self.getResponse(self.telegram)
-        selector = etree.HTML(data)
-        result = selector.xpath("//div[@class='tgme_page_extra']/text()")
-        result = re.sub("[^\d]", "", result[0])
+        data = await self.getResponseAsync(self.telegram)
+        soup=BeautifulSoup(data,'lxml')
+        result=soup.select('.tgme_page_extra')[0].string
+        # result = selector.xpath("//div[@class='tgme_page_extra']/text()")
+        result = re.sub("[^\d]", "", result)
         self.result[tag] = result
-        print('tele over')
+        logging.info('{} tele over {}'.format(self.telegram,result))
 
     def getTwitterFollowers(self):
         tag = "TwitterFollowers"
         if self.twitter == '':
             return
         driver.get(self.twitter)
+
         selector = etree.HTML(driver.page_source)
         followers = selector.xpath('//span[@class="ProfileNav-value" and @data-is-compact="true"]/@data-count')
         if len(followers) == 0:
@@ -200,11 +274,19 @@ class ICOTerm():
         count = followers[0]
         self.result[tag] = count
         print('twitter over')
-    # def renew_connection(self):
-    #     with Controller.from_port(port = 9051) as controller:
-    #         controller.authenticate(password = 'fyl815')
-    #         controller.signal(Signal.NEWNYM)
-    #         controller.close()
+    async def renew_connection(self):
+        with Controller.from_port(port = 9051) as controller:
+            controller.authenticate(password = 'fyl815')
+            controller.signal(Signal.NEWNYM)
+            controller.close()
+
+    @asyncio.coroutine
+    async def writeResult(self):
+        async with AIOFile('ico.json', 'a') as afp:
+            writer = Writer(afp)
+            await writer(self.result)
+            await afp.fsync()
+            logging.info('{} json is writed'.format(self.result['id']))
 	#threads
     def MulTreGetter(self):
         threads = [
@@ -222,28 +304,46 @@ class ICOTerm():
         for t in threads:
             t.join()
         return self.result
+    async def tasks(self):
+        await self.getAlexaRank()
+        await self.getTelegramChannelSize()
+        # await self.renew_connection()
+        await self.getGitHubStar()
+        await self.GetYoutubeViewCount()
 
 
 
 
 if __name__ == "__main__":
-    # t = time.time()
-    # p = Pool(10)#todo
 
-
-
-
-    processes, Terms, res = {}, {}, {}
-    # postData=collections.defaultdict(list)
+    import time,collections
+    start = time.time()
+    Terms={}
+    postData = collections.defaultdict(list)
     file = open('ico_projs.json', "r")
     fileJson = json.load(file)
     for ico_proj in fileJson:
         result = ICOTerm(ico_proj)
         Terms[ico_proj["id"]] = result
-
-    tasks=[v.getAlexaRank() for k,v in Terms.items()]
+    logging.info('Initialize via json')
+    tasks=[v.tasks() for k,v in Terms.items()]
     loop=asyncio.get_event_loop()
     loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
+    # writeTasks=[v.writeResult() for k,v in Terms.items()]
+    # loop.run_until_complete(asyncio.wait(writeTasks))
+    for k,v in Terms.items():
+        postData['data'].append(v.result)
+    postData['date'] = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+    filename = 'ico.json'
+    fp = open(filename, 'a')
+    fp.write(json.dumps(postData))
+    fp.close()
+    logging.info('cost time ' + str(time.time() - start))
+    # t = time.time()
+    # p = Pool(10)#todo
+    # processes, Terms, res = {}, {}, {}
+    # postData=collections.defaultdict(list)
     # #multi-processes
     # for term in Terms.keys():
     #     result = p.apply_async(Terms[term].MulTreGetter)
